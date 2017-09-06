1462da16d96a2c1f26592ef04fb95e30eb1ee5b2
Vinicius Sarmento <vinicius.eng.comp92@gmail.com>
2017-09-05 21:48:44 -0300

    nova tela

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,15 +9,17 @@ import 'rxjs/add/operator/map';
 })
 export class HomePage {
 
-  public feeds: Array<string>;
-  private url: string = "api/test.json";
+  // public feeds: Array<string>;
+  // private url: string = "api/test.json";
+  private url: string;
   // private url: string = "https://www.reddit.com/new.json";
 
   constructor(public navCtrl: NavController, public http: Http) {
-
-    this.http.get(this.url).map(res => res.json()).subscribe(data => {
-        this.feeds = data.data.children;
-    });
- }
+    this.url  = "img/logo.jpg";
+ //
+ //    this.http.get(this.url).map(res => res.json()).subscribe(data => {
+ //        this.feeds = data.data.children;
+ //    });
+  }
 
 }
